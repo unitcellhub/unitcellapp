40b26785831479a8f37c9617f01e7a5d5838ab0a
Ryan Watkins <ryan.t.watkins@jpl.nasa.gov>
2024-11-12 10:17:49 -0800

    Added in gunicorn configuration parameters to restart works after a certain number of requests to help manager memory leaks.

--- docker/gunicorn.config.py
+++ docker/gunicorn.config.py
@@ -1,5 +1,6 @@
-import multiprocessing
 import os
 
-workers = int(os.getenv('WEB_CONCURRENCY', 2))
-threads = int(os.getenv('PYTHON_MAX_THREADS', 8))
+workers = int(os.getenv("WEB_CONCURRENCY", 2))
+threads = int(os.getenv("PYTHON_MAX_THREADS", 8))
+max_requests = int(os.getenv("MAX_REQUESTS", 1000))
+max_requests_jitter = int(os.getenv("MAX_REQUESTS_JITTER", 50))
